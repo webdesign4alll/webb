95a00c2289dff931fdb1bff9bd2db6602f75df9d
webdesign4alll <asbupsilonviroidno1@gmail.com>
2020-09-19 17:56:53 +0530

    Birthdayfile

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Athul Bro"&&b=="My sis")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("NOT A ROBOT");
+  		window.location.href="index.html";
+  	}
+  	
